4ee046f61ff48af9521436b2b500734bdb15f920
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-09-01 22:48:02 +0200

    Add roles/permissions for users

--- services/api/prisma/seed.js
+++ services/api/prisma/seed.js
@@ -1,14 +1,62 @@
 import "dotenv/config"
-import { PrismaClient } from "@prisma/client"
+import { PermissionType, PrismaClient } from "@prisma/client"
 import bcrypt from "bcryptjs"
 
 const prisma = new PrismaClient()
 
 async function main() {
-    const hashedPassword = bcrypt.hash(
+    const hashedPassword = await bcrypt.hash(
         process.env.SEEDING_ADMIN_PASSWORD,
-        process.env.PASSWORD_SALT_LENGTH
+        +process.env.PASSWORD_SALT_LENGTH
     )
+
+    // Create permissions
+    await prisma.$transaction(
+        Object.entries(PermissionType).map(([, p]) =>
+            prisma.permission.upsert({
+                where: {
+                    type: p,
+                },
+                create: {
+                    type: p,
+                },
+                update: {},
+            })
+        )
+    )
+
+    // Create roles
+    await prisma.$transaction([
+        prisma.role.upsert({
+            where: {
+                name: "admin",
+            },
+            update: {},
+            create: {
+                name: "admin",
+                permissions: {
+                    connect: [
+                        { type: PermissionType.CREATE },
+                        { type: PermissionType.READ },
+                        { type: PermissionType.UPDATE },
+                        { type: PermissionType.DELETE },
+                    ],
+                },
+            },
+        }),
+        prisma.role.upsert({
+            where: {
+                name: "user",
+            },
+            update: {},
+            create: {
+                name: "user",
+                permissions: {
+                    connect: [{ type: PermissionType.READ }],
+                },
+            },
+        }),
+    ])
 
     const admin = await prisma.user.upsert({
         where: { email: process.env.SEEDING_ADMIN_EMAIL },
@@ -17,10 +65,13 @@ async function main() {
             email: process.env.SEEDING_ADMIN_EMAIL,
             name: "Admin",
             password: hashedPassword,
+            roles: {
+                connect: { name: "admin" },
+            },
         },
     })
-    console.dir(admin)
 }
+
 main()
     .then(async () => {
         await prisma.$disconnect()
