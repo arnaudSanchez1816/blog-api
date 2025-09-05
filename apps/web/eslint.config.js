f59143c4b62b3ba047f7abdac1052ecb249b59ab
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-09-05 03:09:53 +0200

    Add tanstack router to app

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,3 +1,9 @@
+import { globalIgnores } from "eslint/config"
 import { config } from "@repo/eslint-config/react"
+import pluginRouter from "@tanstack/eslint-plugin-router"
 
-export default config
+export default [
+    ...config,
+    ...pluginRouter.configs["flat/recommended"],
+    globalIgnores(["**/routeTree.gen.ts"]),
+]
--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,12 +1,21 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { HeroUIProvider } from "@heroui/react"
-import App from "./App.jsx"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
+import { routeTree } from "./routeTree.gen"
+import "./style.css"
+
+// Set up a Router instance
+const router = createRouter({
+    routeTree,
+    defaultPreload: "intent",
+    scrollRestoration: true,
+})
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <HeroUIProvider>
-            <App />
+            <RouterProvider router={router} />
         </HeroUIProvider>
     </StrictMode>
 )
