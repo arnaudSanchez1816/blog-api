a09a941d1998a221f6f8f8b5cfeb4c287e87211d
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-09-10 01:19:33 +0200

    Move search bar to home aside

--- apps/web/src/components/Header.jsx
+++ apps/web/src/components/Header.jsx
@@ -12,6 +12,7 @@ import {
 import { useState } from "react"
 import { useLocation } from "react-router"
 import NavLink from "./NavLink"
+import SearchIcon from "./SearchIcon"
 
 function BlogLogo({ ...props }) {
     return (
@@ -22,36 +23,6 @@ function BlogLogo({ ...props }) {
             {...props}
         >
             <path d="M180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h462l198 198v462q0 24-18 42t-42 18H180Zm0-60h600v-428.57H609V-780H180v600Zm99-111h402v-60H279v60Zm0-318h201v-60H279v60Zm0 159h402v-60H279v60Zm-99-330v171.43V-780v600-600Z" />
-        </svg>
-    )
-}
-
-function SearchIcon({ size = 24, strokeWidth = 1.5, width, height, ...props }) {
-    return (
-        <svg
-            aria-hidden="true"
-            fill="none"
-            focusable="false"
-            height={height || size}
-            role="presentation"
-            viewBox="0 0 24 24"
-            width={width || size}
-            {...props}
-        >
-            <path
-                d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={strokeWidth}
-            />
-            <path
-                d="M22 22L20 20"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={strokeWidth}
-            />
         </svg>
     )
 }
@@ -101,7 +72,7 @@ export default function Header() {
             </NavbarContent>
 
             <NavbarContent
-                className="hidden gap-4 font-medium sm:flex"
+                className="hidden grow gap-4 font-medium sm:flex"
                 justify="center"
             >
                 <NavbarItem isActive={locationPathname === "/posts"}>
@@ -125,21 +96,7 @@ export default function Header() {
                     </NavLink>
                 </NavbarItem>
             </NavbarContent>
-            <NavbarContent justify="end" as="div">
-                <Input
-                    classNames={{
-                        base: "max-w-full sm:max-w-[10rem] h-10",
-                        mainWrapper: "h-full",
-                        input: "text-xs md:text-sm",
-                        inputWrapper:
-                            "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-                    }}
-                    placeholder="Search posts"
-                    size="sm"
-                    startContent={<SearchIcon size={18} />}
-                    type="search"
-                />
-            </NavbarContent>
+            <NavbarContent></NavbarContent>
             <NavbarMenu>
                 <NavbarMenuItem>
                     <Link
--- apps/web/src/pages/Home.jsx
+++ apps/web/src/pages/Home.jsx
@@ -2,7 +2,8 @@ import { useLoaderData } from "react-router"
 import { getPublicPosts } from "../api/posts"
 import PostPreview from "../components/PostPreview"
 import { getTags } from "../api/tags"
-import { Button, Link } from "@heroui/react"
+import { Button, Input, Link } from "@heroui/react"
+import SearchIcon from "../components/SearchIcon"
 
 export async function homeLoader() {
     const [posts, tags] = await Promise.all([
@@ -26,17 +27,42 @@ export default function Home() {
 
     return (
         <div className="mx-auto pt-6 xl:grid xl:grid-cols-[1fr_auto_1fr] xl:justify-center xl:gap-x-16">
-            <div className="m-auto max-w-prose xl:col-start-2">
-                <h1 className="text-3xl font-bold">Latest posts</h1>
-                <div className="mt-6">
-                    {allPosts.map((post) => (
-                        <PostPreview post={post} key={post.id} />
-                    ))}
-                </div>
+            <div className="m-auto max-w-prose xl:m-0 xl:justify-self-end">
+                <h1 className="text-3xl font-medium">Latest posts</h1>
+            </div>
+            <div className="m-auto mt-8 max-w-prose xl:mt-0">
+                {allPosts.map((post) => (
+                    <PostPreview
+                        post={post}
+                        key={post.id}
+                        className="[&+*]:mt-12"
+                    />
+                ))}
             </div>
             <aside className="mx-auto mt-12 max-w-prose xl:mx-0 xl:mt-0 xl:max-w-xs">
-                <div>
-                    <h3 className="text-xl font-bold">All tags</h3>
+                <form className="flex flex-nowrap gap-4">
+                    <Input
+                        classNames={{
+                            input: "text-sm",
+                            inputWrapper:
+                                "font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20 h-[40px]",
+                            label: "text-lg font-medium",
+                        }}
+                        size="sm"
+                        startContent={<SearchIcon size={18} />}
+                        type="search"
+                        label="Find a post"
+                        labelPlacement="outside-top"
+                    />
+                    <Button
+                        radius="sm"
+                        className="bg-default-900 text-content1 h-[40px] self-end"
+                    >
+                        Search
+                    </Button>
+                </form>
+                <div className="mt-8">
+                    <h3 className="text-lg font-medium">All tags</h3>
                     <div className="mt-6 flex max-w-full flex-wrap gap-2">
                         {allTags.map((tag) => (
                             <Button
@@ -45,7 +71,7 @@ export default function Home() {
                                 as={Link}
                                 href={`/search?tag=${tag.id}`}
                                 size="sm"
-                                radius="full"
+                                radius="sm"
                             >
                                 {tag.name}
                             </Button>
