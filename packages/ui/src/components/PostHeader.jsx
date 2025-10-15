e4b4a04d579e49bec7a00092d63ed5bb6e024090
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-10-16 00:21:54 +0200

    Fix render issue for unpublished posts

--- packages/ui/src/components/PostHeader.jsx
+++ packages/ui/src/components/PostHeader.jsx
@@ -8,13 +8,17 @@ export default function PostHeader({ post }) {
             <h1 className="text-3xl font-medium">{title}</h1>
             <div className="text-foreground/70 my-4 flex flex-col gap-4 text-sm">
                 <div className="flex gap-2">
-                    <time dateTime="2025-01-01">
-                        {format(publishedAt, "MMMM dd, y")}
-                    </time>
+                    {publishedAt ? (
+                        <time dateTime={publishedAt}>
+                            {format(publishedAt, "MMMM dd, y")}
+                        </time>
+                    ) : (
+                        <p className="text-warning">Unpublished</p>
+                    )}
                     <span>•</span>
                     <span>{`${readingTime} min read`}</span>
                 </div>
-                {tags.length > 0 && (
+                {tags && tags.length > 0 && (
                     <div className="flex max-w-full flex-wrap gap-2">
                         {tags.map((tag) => (
                             <Tag key={tag.id} tag={tag} />
