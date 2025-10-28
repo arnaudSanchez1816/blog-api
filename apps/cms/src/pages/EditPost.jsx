62d4342388d9fa4180d91fd2f5b7627fc80c5c59
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-10-28 20:39:42 +0100

    Improve post editing section

--- apps/cms/src/pages/EditPost.jsx
+++ apps/cms/src/pages/EditPost.jsx
@@ -1,13 +1,21 @@
 import { fetchPost } from "@repo/client-api/posts"
 import { postSchema } from "@repo/zod-schemas"
-import { Form, useLoaderData, useNavigation, useSubmit } from "react-router"
-import { useState } from "react"
-import { Button, Divider, Textarea } from "@heroui/react"
-import PostMarkdown from "@repo/ui/components/PostMarkdown"
+import {
+    Form,
+    useBlocker,
+    useLoaderData,
+    useNavigation,
+    useSubmit,
+} from "react-router"
+import { useCallback, useState } from "react"
+import { Button, Divider } from "@heroui/react"
 import { fetchTags } from "@repo/client-api/tags"
 import _ from "lodash"
 import EditablePostTitle from "../components/EditablePostTitle"
 import EditTagsSection from "../components/EditTagsSection"
+import EditPostContentSection from "../components/EditPostContentSection"
+import ThreeColumnLayout from "../layouts/ThreeColumnLayout"
+import SaveIcon from "@repo/ui/components/Icons/SaveIcon"
 
 export async function editPostLoader({ params }, accessToken) {
     const postIdSchema = postSchema.pick({ id: true })
@@ -20,12 +28,16 @@ export async function editPostLoader({ params }, accessToken) {
     return { post, allTags: allTags.results }
 }
 
+function EditPostLayout({ children, left, right }) {
+    return <ThreeColumnLayout left={left} right={right} center={children} />
+}
+
 export default function EditPost() {
     const { post } = useLoaderData()
-    const { id, body } = post
-    const [textValue, setTextValue] = useState(body)
-    const submit = useSubmit()
+    const [isDirty, setIsDirty] = useState(false)
+    const blocker = useBlocker(useCallback(() => isDirty, [isDirty]))
     const navigation = useNavigation()
+    const submit = useSubmit()
 
     const isSaving = navigation.state !== "idle"
 
@@ -34,49 +46,31 @@ export default function EditPost() {
     }
 
     return (
-        <div className="pt-6">
-            <div className="mx-auto max-w-prose">
-                <EditablePostTitle post={post} />
-                <div className="mt-4">
-                    <EditTagsSection post={post} />
-                </div>
-                <Divider className="mt-4" />
-            </div>
-            <div className="mx-auto mt-8 grid w-fit grid-cols-[repeat(2,65ch)] grid-rows-[repeat(3,auto)] gap-x-24 gap-y-4">
-                <h3 className="text-2xl font-medium">Post content</h3>
-                <h4 className="text-2xl font-medium">Preview</h4>
-                <div>
-                    <Textarea
-                        value={textValue}
-                        aria-label="Post edit area"
-                        variant="faded"
-                        minRows={25}
-                        maxRows={25}
-                        onValueChange={setTextValue}
-                        size="lg"
-                    ></Textarea>
-                </div>
-                <div className="max-h-[600px] overflow-y-scroll">
-                    <PostMarkdown>{textValue}</PostMarkdown>
-                </div>
-                <div className="col-span-2">
-                    <Form
-                        method="PUT"
-                        action={`/posts/${id}`}
-                        onSubmit={onSave}
+        <EditPostLayout
+            left={
+                <div className="min-w-38">
+                    <p className="text-2xl font-medium xl:text-3xl">
+                        Edit post
+                    </p>
+                    <Button
+                        isLoading={isSaving}
+                        color="primary"
+                        size="md"
+                        className="min-sm:max-xl:max-w-32 mt-4 w-full font-medium"
+                        onPress={onSave}
+                        startContent={<SaveIcon />}
                     >
-                        <Button
-                            isLoading={isSaving}
-                            type="submit"
-                            color="primary"
-                            size="lg"
-                            className="font-medium"
-                        >
-                            Save
-                        </Button>
-                    </Form>
+                        Save
+                    </Button>
                 </div>
+            }
+        >
+            <EditablePostTitle post={post} />
+            <div className="mt-4">
+                <EditTagsSection post={post} />
             </div>
-        </div>
+            <Divider className="mt-4" />
+            <EditPostContentSection post={post} />
+        </EditPostLayout>
     )
 }
