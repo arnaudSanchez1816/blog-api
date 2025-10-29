759d845b0d93c153035c1e31b62aced696ac6454
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-10-30 00:56:59 +0100

    Fix wrong component name

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -69,7 +69,7 @@ const router = createBrowserRouter(
             </Route>
             <Route
                 path="/*"
-                errorElement={<ErrorView />}
+                errorElement={<Error />}
                 loader={() => {
                     throw data("Page not found", 404)
                 }}
