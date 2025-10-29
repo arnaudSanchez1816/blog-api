c6f077ec99db2fa372585f2ebf5dda99617f084f
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-10-30 00:57:10 +0100

    Fix component import path

--- apps/web/src/pages/About.jsx
+++ apps/web/src/pages/About.jsx
@@ -1,7 +1,6 @@
 import { useLoaderData } from "react-router"
 import aboutMarkdownUrl from "../assets/about.md"
-import Markdown from "react-markdown"
-import PostMarkdown from "@repo/ui/components/PostMarkdown"
+import PostMarkdown from "@repo/ui/components/posts/PostMarkdown"
 
 export const aboutLoader = async () => {
     const response = await fetch(aboutMarkdownUrl)
