18f735b68faec0a4414de01e0d46abf001fd9a2a
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-11-11 20:24:01 +0100

    Remove not working module path alias config

--- services/api/eslint.config.js
+++ services/api/eslint.config.js
@@ -1,11 +1,9 @@
 import { config } from "@repo/eslint-config/base"
 import { defineConfig } from "vitest/config"
 import vitest from "@vitest/eslint-plugin"
-import { importX } from "eslint-plugin-import-x"
 
 export default defineConfig([
     ...config,
-    ...importX.flatConfigs.recommended,
     {
         files: ["src/**/*.test.js"],
         plugins: {
