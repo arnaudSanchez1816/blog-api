c488f12511497948494a4e1e7ab0ca75f8d4b0da
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-12-12 01:12:13 +0100

    Modify signupUser method parameter dto name

--- services/api/src/signup/signupService.ts
+++ services/api/src/signup/signupService.ts
@@ -1,14 +1,14 @@
 import type { Prisma } from "@prisma/client"
 import * as userService from "../users/usersService.js"
 
-type CreateUserDTO = Prisma.UserGetPayload<{
+type SignupUserDTO = Prisma.UserGetPayload<{
     select: {
         email: true
         name: true
         password: true
     }
 }>
-export const signupUser = async ({ email, name, password }: CreateUserDTO) => {
+export const signupUser = async ({ email, name, password }: SignupUserDTO) => {
     const newUser = await userService.createUser({
         email,
         name,
